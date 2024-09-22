c5f7aa2b520ae2c55d4a9540eaa79caca2586a1e
Pheubel <luca.guagliardo@live.nl>
2024-09-22 16:11:58 +0200

    add small delay to keep scene switch working

--- Project/scene_manager/scene_transition_base.gd
+++ Project/scene_manager/scene_transition_base.gd
@@ -7,7 +7,9 @@ signal post_switch_phase_finished
 var transition_arguments := {}
 
 func start_pre_switch_phase() -> void:
+	await get_tree().process_frame
 	pre_switch_phase_finished.emit()
 
 func start_post_switch_phase() -> void:
+	await get_tree().process_frame
 	post_switch_phase_finished.emit()
